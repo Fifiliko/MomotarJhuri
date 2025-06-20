047f8a9070d867005d12715ef8628ce2d423ef29
Tofa <thofazzolhossen@gmail.com>
2025-06-20 18:52:35 +0600

    Add project files.

---
+++ MomotarJhuri.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MomotarJhuri.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
